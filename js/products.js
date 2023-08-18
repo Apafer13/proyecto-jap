d28800fdcfec416215c6af0f35c0f298483a178e
Apafer13 <apafer11@gmail.com>
2023-08-18 19:10:56 -0300

    trabajo terminado

--- js/products.js
+++ js/products.js
@@ -1,15 +1,15 @@
-
 const URL_Autos = "https://japceibal.github.io/emercado-api/cats_products/101.json";
-const auto = document.getElementById("cat-list-container");
-function showData(Array){
+
+function showData(Array) {
+    let auto = document.getElementById("cat-list-container");
     let htmlContentToAppend = "";
-    for(const item of Array){
-        htmlContentToAppend +=`
+    for (const item of Array) {
+        htmlContentToAppend += `
         <div class="list-group-item list-group-item-action cursor-active">
-        <div class="row">
-            <div class="col-3">
-                <img src="${item.image}" class="img-thumbnail">
-            </div>
+            <div class="row">
+                <div class="col-3">
+                    <img src="${item.image}" class="img-thumbnail">
+                </div>
             <div class="col">
                 <div class="d-flex w-100 justify-content-between">
                     <h4 class="mb-1">${item.name}</h4>
@@ -17,12 +17,12 @@ function showData(Array){
                 </div>
                 <p class="mb-1">${item.description}</p>
                 </div>
-                </div>
-     </div>`
-    auto.innerHTML = htmlContentToAppend;
+            </div>
+         </div>`
+        auto.innerHTML = htmlContentToAppend;
     }
 }
 
 fetch(URL_Autos)
-.then (response => response.json())
-.then (data => showData(data.products))
+    .then(response => response.json())
+    .then(data => showData(data.products))
